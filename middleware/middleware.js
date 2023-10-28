ba581ec95c6c695790bc9832518fc6976470de74
rasyadpras <106673670+rasyadpras@users.noreply.github.com>
2023-10-28 14:09:09 +0700

    readme file + update middleware

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -34,6 +34,23 @@ function checkPostAccount(req, res, next) {
     next();
 };
 
+function checkPostTransaction(req, res, next) {
+    const schema = Joi.object({
+        user_id: Joi.number().required(),
+        bank_name: Joi.string().required(),
+        bank_account_number: Joi.number().required(),
+        balance: Joi.number().precision(2).required()
+    });
+    const { error } = schema.validate(req.body);
+    if (error) {
+        let resp = ResponseTemplate(null, 'invalid request',
+            error.details[0].message, 400)
+        res.json(resp)
+        return;
+    };
+    next();
+};
+
 module.exports = {
     checkPostUser,
     checkPostAccount
