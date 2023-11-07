d8bfdc9645b7c0f215a31e181d66fcd7f01f0f3f
rasyadpras <106673670+rasyadpras@users.noreply.github.com>
2023-11-07 22:44:13 +0700

    testing update

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,50 +1,38 @@
 const request = require('supertest');
 const app = require('../index');
 
-describe('GET /api/v1/users', () => {
+describe('user test', () => {
     it('get all user data', async () => {
-        request(app)
-            .get('/api/v1/users')
-            .then(res => {
-                expect(res.statusCode).toBe(200);
-                expect(res.body).toHaveProperty('data');
-                expect(res.body).toHaveProperty('message');
-                expect(res.body).toHaveProperty('status');
-                expect(res.body.status).toEqual(200);
-                expect(res.body.message).toEqual('get data success');
-            })
-        done();
-    })
+        const resp = await request(app).get('/api/v1/users');
+        expect(resp.statusCode).toBe(200);
+        expect(resp.body.data).toBeTruthy();
+        expect(resp.body).toHaveProperty('message');
+        expect(resp.body).toHaveProperty('status');
+        expect(resp.body.status).toEqual(200);
+        expect(resp.body.message).toEqual('get data success');
+    });
 });
 
-describe('GET /api/v1/accounts', () => {
-    test('return 200', done => {
-        request(app)
-            .get('/api/v1/accounts')
-            .then(res => {
-                expect(res.statusCode).toBe(200);
-                expect(res.body).toHaveProperty('data');
-                expect(res.body).toHaveProperty('message');
-                expect(res.body).toHaveProperty('status');
-                expect(res.body.status).toEqual(200);
-                expect(res.body.message).toEqual('get data success');
-            })
-        done();
-    })
+describe('account test', () => {
+    it('get all user account', async () => {
+        const resp = await request(app).get('/api/v1/accounts');
+        expect(resp.statusCode).toBe(200);
+        expect(resp.body.data).toBeTruthy();
+        expect(resp.body).toHaveProperty('message');
+        expect(resp.body).toHaveProperty('status');
+        expect(resp.body.status).toEqual(200);
+        expect(resp.body.message).toEqual('get data success');
+    });
 });
 
-describe('GET /api/v1/transactions', () => {
-    test('return 200', done => {
-        request(app)
-            .get('/api/v1/transactions')
-            .then(res => {
-                expect(res.statusCode).toBe(200);
-                expect(res.body).toHaveProperty('data');
-                expect(res.body).toHaveProperty('message');
-                expect(res.body).toHaveProperty('status');
-                expect(res.body.status).toEqual(200);
-                expect(res.body.message).toEqual('get data success');
-            })
-        done();
-    })
+describe('transaction test', () => {
+    it('get all transaction data', async () => {
+        const resp = await request(app).get('/api/v1/transactions');
+        expect(resp.statusCode).toBe(200);
+        expect(resp.body.data).toBeTruthy();
+        expect(resp.body).toHaveProperty('message');
+        expect(resp.body).toHaveProperty('status');
+        expect(resp.body.status).toEqual(200);
+        expect(resp.body.message).toEqual('get data success');
+    });
 });
