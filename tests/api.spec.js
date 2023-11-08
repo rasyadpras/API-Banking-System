2e4565ae3771f452cf46432c2c81c06a5b663e6f
rasyadpras <106673670+rasyadpras@users.noreply.github.com>
2023-11-08 15:05:42 +0700

    add testing post and get by id

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -11,6 +11,35 @@ describe('user test', () => {
         expect(resp.body.status).toEqual(200);
         expect(resp.body.message).toEqual('get data success');
     });
+
+    it('get user data by id', async () => {
+        const id = 1;
+        const resp = await request(app).get(`/api/v1/users/${id}`);
+        expect(resp.statusCode).toBe(200);
+        expect(resp.body.data).toBeTruthy();
+        expect(resp.body).toHaveProperty('message');
+        expect(resp.body).toHaveProperty('status');
+        expect(resp.body.status).toEqual(200);
+        expect(resp.body.message).toEqual('get data success');
+    });
+
+    it('add new user', async () => {
+        const testUser = {
+            name: "Test User",
+            email: "test@mail.com",
+            password: "password123",
+            identity_type: "Identity card",
+            identity_number: "0011223344",
+            address: "Anywhere"
+        };
+        const resp = await request(app).post('/api/v1/users').send(testUser);
+        expect(resp.statusCode).toBe(201);
+        expect(resp.body.data).toBeTruthy();
+        expect(resp.body).toHaveProperty('message');
+        expect(resp.body).toHaveProperty('status');
+        expect(resp.body.status).toEqual(201);
+        expect(resp.body.message).toEqual('input data success');
+    });
 });
 
 describe('account test', () => {
@@ -23,6 +52,33 @@ describe('account test', () => {
         expect(resp.body.status).toEqual(200);
         expect(resp.body.message).toEqual('get data success');
     });
+
+    it('get user account by id', async () => {
+        const id = 1;
+        const resp = await request(app).get(`/api/v1/accounts/${id}`);
+        expect(resp.statusCode).toBe(200);
+        expect(resp.body.data).toBeTruthy();
+        expect(resp.body).toHaveProperty('message');
+        expect(resp.body).toHaveProperty('status');
+        expect(resp.body.status).toEqual(200);
+        expect(resp.body.message).toEqual('get data success');
+    });
+
+    it('add new account', async () => {
+        const testAccount = {
+            user_id: 1,
+            bank_name: 'Example Bank',
+            bank_account_number: 1001,
+            balance: 1000
+        };
+        const resp = await request(app).post('/api/v1/accounts').send(testAccount);
+        expect(resp.statusCode).toBe(201);
+        expect(resp.body.data).toBeTruthy();
+        expect(resp.body).toHaveProperty('message');
+        expect(resp.body).toHaveProperty('status');
+        expect(resp.body.status).toEqual(201);
+        expect(resp.body.message).toEqual('input data success');
+    });
 });
 
 describe('transaction test', () => {
@@ -34,5 +90,31 @@ describe('transaction test', () => {
         expect(resp.body).toHaveProperty('status');
         expect(resp.body.status).toEqual(200);
         expect(resp.body.message).toEqual('get data success');
+    });
+
+    it('get transaction data by id', async () => {
+        const id = 1;
+        const resp = await request(app).get(`/api/v1/transactions/${id}`);
+        expect(resp.statusCode).toBe(200);
+        expect(resp.body.data).toBeTruthy();
+        expect(resp.body).toHaveProperty('message');
+        expect(resp.body).toHaveProperty('status');
+        expect(resp.body.status).toEqual(200);
+        expect(resp.body.message).toEqual('get data success');
+    });
+
+    it('add new transaction', async () => {
+        const testTransaction = {
+            source_account_id: 1,
+            destination_account_id: 2,
+            amount: 1000
+        };
+        const resp = await request(app).post('/api/v1/transactions').send(testTransaction);
+        expect(resp.statusCode).toBe(201);
+        expect(resp.body.data).toBeTruthy();
+        expect(resp.body).toHaveProperty('message');
+        expect(resp.body).toHaveProperty('status');
+        expect(resp.body.status).toEqual(201);
+        expect(resp.body.message).toEqual('input data success');
     });
 });
